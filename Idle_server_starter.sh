39b09b91f2fc59755979d0bfdca12890bafb41f0
hamzathe3 <124628723+markthe3@users.noreply.github.com>
2023-08-08 14:48:29 +0100

    Create Idle_server_starter.sh

---
+++ Idle_server_starter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set the path to your monitoring script
+monitoring_script="/path/to/idle_server.sh"
+
+# Function to check if the monitoring script is already running
+function check_monitoring_script {
+    pgrep -f "$monitoring_script" > /dev/null
+    return $?
+}
+
+# Check if the monitoring script is already running
+check_monitoring_script
+if [ $? -eq 0 ]; then
+    echo "Monitoring script is already running."
+else
+    echo "Starting monitoring script in the background..."
+    nohup $monitoring_script > /dev/null 2>&1 &
+    echo "Monitoring script is running in the background."
+fi
